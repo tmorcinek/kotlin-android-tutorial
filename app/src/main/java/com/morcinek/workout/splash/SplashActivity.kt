867145f3d0058d82c89b60f540e4d931da53c1a1
Tomasz Morcinek <tomasz@morcinek.co.uk>
2017-10-11 22:04:06 +0200

    Fixing SplashActivity according to Google guidelines.

--- app/src/main/java/com/morcinek/workout/splash/SplashActivity.kt
+++ app/src/main/java/com/morcinek/workout/splash/SplashActivity.kt
@@ -3,22 +3,14 @@ package com.morcinek.workout.splash
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.morcinek.workout.HomeActivity
-import com.morcinek.workout.R
-import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.uiThread
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash)
 
-        doAsync {
-            Thread.sleep(2000)
-            uiThread {
-                startActivity<HomeActivity>()
-            }
-        }
+        startActivity<HomeActivity>()
+        finish()
     }
 }
