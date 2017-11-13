46f43fb18ad67546ff5f4ecbeef10672133dca4d
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2017-11-13 12:24:09 +0100

    Elvis Operator, Equality.

--- app/src/main/java/com/morcinek/workout/common/fragment/ContentFragmentManager.kt
+++ app/src/main/java/com/morcinek/workout/common/fragment/ContentFragmentManager.kt
@@ -8,12 +8,17 @@ class ContentFragmentManager(private val activity: FragmentActivity, private val
 
     private val fragmentManager by lazy { activity.supportFragmentManager }
 
+    val currentFragment: Fragment?
+        get() = fragmentManager.fragments.firstOrNull { it?.isVisible ?: false }
+
     fun replaceFragment(fragment: Fragment, tag: String = fragment.javaClass.name, addToBackStack: Boolean = false) {
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(contentLayoutId, fragment, tag)
-        if (addToBackStack) {
-            fragmentTransaction.addToBackStack(tag)
+        if (currentFragment?.javaClass != fragment.javaClass) {
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(contentLayoutId, fragment, tag)
+            if (addToBackStack) {
+                fragmentTransaction.addToBackStack(tag)
+            }
+            fragmentTransaction.commit()
         }
-        fragmentTransaction.commit()
     }
 }
