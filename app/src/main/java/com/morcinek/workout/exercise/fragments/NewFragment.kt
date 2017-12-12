12c003eaecc1a9b4f0330b8c91d9a8c8ec70e86c
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2017-12-13 00:13:30 +0100

    Refactoring layout in NewFragment.

--- app/src/main/java/com/morcinek/workout/exercise/fragments/NewFragment.kt
+++ app/src/main/java/com/morcinek/workout/exercise/fragments/NewFragment.kt
@@ -6,15 +6,12 @@ import android.view.View
 import android.widget.ArrayAdapter
 import com.morcinek.workout.R
 import com.morcinek.workout.common.fragment.BaseFragment
-import com.morcinek.workout.common.utils.dateFormat
 import com.morcinek.workout.common.utils.formatWith
 import com.morcinek.workout.common.utils.timeFormat
 import com.morcinek.workout.core.data.exercises.ExerciseDataModel
-import com.morcinek.workout.core.data.exercises.ExerciseManager
 import com.morcinek.workout.exercise.ExerciseDataManager
 import com.morcinek.workout.exercise.exerciseComponent
 import kotlinx.android.synthetic.main.exercise_new.*
-import org.jetbrains.anko.sdk25.coroutines.onItemClick
 import org.jetbrains.anko.sdk25.coroutines.onItemSelectedListener
 import org.jetbrains.anko.sdk25.coroutines.textChangedListener
 import java.util.*
@@ -26,9 +23,7 @@ class NewFragment : BaseFragment() {
     override val layoutResourceId = R.layout.exercise_new
 
     @Inject lateinit var exerciseDataManager: ExerciseDataManager
-    @Inject lateinit var exerciseManager: ExerciseManager
 
-    private val dateFormat by lazy { dateFormat() }
     private val timeFormat by lazy { timeFormat() }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
@@ -36,34 +31,39 @@ class NewFragment : BaseFragment() {
         exerciseComponent.inject(this)
 
         val exerciseDataModel = exerciseDataManager.exerciseDataModel
+        setupName(exerciseDataModel)
+        setupDate(exerciseDataModel)
+        setupCategory(exerciseDataModel)
+
+        breakButton.setOnClickListener { exerciseDataManager.showBreak() }
+        exerciseButton.setOnClickListener { exerciseDataManager.showSeries() }
+    }
+
+    private fun setupName(exerciseDataModel: ExerciseDataModel) {
         nameEditText.setText(exerciseDataModel.name)
         nameEditText.textChangedListener {
             onTextChanged { charSequence, _, _, _ ->
                 exerciseDataModel.name = charSequence.toString()
             }
         }
-        setupCategorySpinner(exerciseDataModel)
+    }
 
+    private fun setupDate(exerciseDataModel: ExerciseDataModel) {
         dateTextView.text = formatDate(exerciseDataModel.date)
-        breakButton.setOnClickListener { exerciseDataManager.showBreak() }
-        exerciseButton.setOnClickListener { exerciseDataManager.showSeries() }
     }
 
-    private fun setupCategorySpinner(exerciseDataModel: ExerciseDataModel) {
-        val adapter = ArrayAdapter.createFromResource(context, R.array.categories, android.R.layout.simple_spinner_item)
-        categorySpinner.adapter = adapter
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        categorySpinner.setSelection(adapter.getPosition(exerciseDataModel.category))
-        categorySpinner.onItemSelectedListener {
-            onItemSelected { _, _, position, _ -> exerciseDataModel.category = adapter.getItem(position).toString()  }
-        }
-    }
-
-    private fun formatDate(date: Calendar) = "${timeFormat(date)}, ${relativeDateFormat(date)}"
-
-    private fun timeFormat(date: Calendar) = date.formatWith(timeFormat)
+    private fun formatDate(date: Calendar) = "${relativeDateFormat(date)}, ${date.formatWith(timeFormat)}"
 
     private fun relativeDateFormat(date: Calendar)
             = DateUtils.getRelativeTimeSpanString(date.timeInMillis, Date().time, DateUtils.DAY_IN_MILLIS, DateUtils.FORMAT_SHOW_YEAR).toString()
 
+    private fun setupCategory(exerciseDataModel: ExerciseDataModel) {
+        val adapter = ArrayAdapter.createFromResource(context, R.array.categories, R.layout.spinner_layout)
+        categorySpinner.adapter = adapter
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        categorySpinner.setSelection(adapter.getPosition(exerciseDataModel.category))
+        categorySpinner.onItemSelectedListener {
+            onItemSelected { _, _, position, _ -> exerciseDataModel.category = adapter.getItem(position).toString() }
+        }
+    }
 }
